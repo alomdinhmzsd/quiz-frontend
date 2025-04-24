f8e25d8bf11bba49740dce63e3d618b5adc93276
padmin <padmin@bulldog.internal>
2025-04-24 18:29:08 -0500

    final final project clean and functioning 6:31

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-🎉🎊🥂🎁
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
